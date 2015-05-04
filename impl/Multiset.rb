a54fa9276a2de7cae208030e26e4b36b6f2423dc
Beatriz Santos <bsantosb@snu.ac.kr>
2015-05-04 20:41:34 +0900

    Multiset first commit

--- impl/Multiset.rb
+++ impl/Multiset.rb
@@ -1,1 +1,18 @@
+class Multiset
 
+# def initialize?
+   def initialize(set)
+       @entries = {}
+       @set = set
+   end
+ 
+# def contains? 
+   def contains? item
+      
+   end
+
+# def contains(n)?
+
+
+
+end
